461e0f9d7600aaad2b855fc993f154fcfc69f82c
Derrick Dymock <derrick@puppetlabs.com>
2013-04-11 12:46:58 -0700

    Style changes.
    Fix the init.pp to match the style guide.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class pe_puppetdb {
   include pe_puppetdb::params
 
   class { 'puppetdb':
-    listen_address => $pe_puppetdb::params::listen_address,
-    ssl_listen_address => $pe_puppetdb::params::ssl_listen_address,
+    listen_address      => $pe_puppetdb::params::listen_address,
+    ssl_listen_address  => $pe_puppetdb::params::ssl_listen_address,
   }
 }
